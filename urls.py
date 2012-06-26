45728d56a94511bb13664d483102e0f3fd3c0d37
Seth House <seth@eseth.com>
2012-06-26 00:20:30 -0700

    Added custom redirect view for old Google links
    Google is still linking to docs on the base .org url instead of
    docs.saltstack.org. This adds a redirect to get those working again.

--- urls.py
+++ urls.py
@@ -1,13 +1,29 @@
 from django.conf.urls.defaults import patterns, include, url
 from django.contrib import admin
+from django.views.generic import RedirectView
 
 from saltstack_org.saltutil import views
 
 admin.autodiscover()
 
+class OldDocs(RedirectView):
+    def get_redirect_url(self, path):
+        rtdurl = '{0}/{1}'.format(self.url, path.lstrip('/'))
+
+        if rtdurl.endswith('/'):
+            return '{0}index.html'.format(rtdurl)
+
+        return rtdurl
+
 urlpatterns = patterns('django.views.generic.simple',
     url(r'^documentation/$', 'redirect_to',
         {'url': 'http://docs.saltstack.org'}, name='documentation'),
+
+    url(r'^ref/(?P<path>.*)$',
+        OldDocs.as_view(url='http://docs.saltstack.org/en/latest/ref')),
+
+    url(r'^topics/(?P<path>.*)$',
+        OldDocs.as_view(url='http://docs.saltstack.org/en/latest/topics')),
 
     # Legacy URL from Sphinx homepage
     url(r'home', 'redirect_to', {'url': '/'}, name='home'),
